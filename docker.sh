2c13c655b8e15212dacffdd762445ed0a64b2b38
Soti Sima <soti.sima@UK03961.lan>
2018-05-12 22:45:02 +0100

    aa

--- docker.sh
+++ docker.sh
@@ -8,3 +8,15 @@ docker run -d \
         --memory="300m" \
         -p 1234:5000 pythonf
 
+# docker ps -a
+# docker login --username=yourhubusername 
+# docker images
+# docker tag 6d9d107555c3 simasware/pythonf:latest
+# docker push 244560807427.dkr.ecr.eu-west-2.amazonaws.com/imgs-soti:latest
+
+# aws ecr get-login --no-include-email --region eu-west-2
+# 
+# docker tag 6d9d107555c3 244560807427.dkr.ecr.eu-west-2.amazonaws.com/imgs-soti:latest
+# docker push 244560807427.dkr.ecr.eu-west-2.amazonaws.com/imgs-soti:latest
+
+
